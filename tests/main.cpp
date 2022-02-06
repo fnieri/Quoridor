3e98317552ce7c807796a82f9f1df7a198200350
bpetrov2020 <boris.petrov@ulb.be>
2022-02-06 10:22:19 +0100

    Little improvements to CMake

--- tests/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch_session.hpp>
-#include "Test_PasswordEncrypter.cpp"
-
-int main( int argc, char* argv[] ) {
-  int result = Catch::Session().run( argc, argv );
-  return result;
-}
