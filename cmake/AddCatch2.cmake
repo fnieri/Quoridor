ce20eb406753c6772b1b80209950b1389c0710ef
bpetrov2020 <boris.petrov@ulb.be>
2022-02-16 21:50:16 +0100

    Use release zips
    Signed-off-by: bpetrov2020 <boris.petrov@ulb.be>

--- cmake/AddCatch2.cmake
+++ cmake/AddCatch2.cmake
@@ -1,1 +1,5 @@
-CPMAddPackage("gh:catchorg/Catch2@2.13.8")
+CPMAddPackage(
+    NAME catch2
+    VERSION 2.13.8
+    URL https://github.com/catchorg/Catch2/archive/refs/tags/v2.13.8.zip
+)
--- cmake/Addjson.cmake
+++ cmake/Addjson.cmake
@@ -1,1 +1,6 @@
-CPMAddPackage("gh:nlohmann/json@3.10.5")
+CPMAddPackage(
+    NAME json
+    VERSION 3.10.5
+    URL https://github.com/nlohmann/json/archive/refs/tags/v3.10.5.zip
+)
+
