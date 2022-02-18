4d60d0cbb28a1a6cd8b6254f01a18b8db94680f8
bpetrov2020 <boris.petrov@ulb.be>
2022-02-18 17:10:55 +0100

    Add GameHandler
    Signed-off-by: bpetrov2020 <boris.petrov@ulb.be>

--- src/server/GameHandler.h
+++ src/server/GameHandler.h
@@ -1,28 +1,52 @@
+/**
+ * @author Boris Petrov
+ */
+
 #pragma once
 
-class GameHandler
+#include "UserHandler.h"
+#include "src/common/Observer.h"
+
+class GameHandler : public Subject
 {
 private:
-    bool m_isPlaying;
-    std::vector<std::shared_ptr<UserHandler>> m_players;
+    bool m_isPlaying {false};
+    UserHub *m_userHub;
+    std::vector<std::string> m_players;
 
 public:
-    GameHandler(std::shared_ptr<UserHandler>, std::shared_ptr<UserHandler>);
+    GameHandler(UserHub *, std::initializer_list<std::string>);
+
+    bool isFinished() const
+    {
+        return m_isPlaying;
+    }
+
+    /**
+     * In order to relay the message only to those needing it (i.e.
+     * not the sender), the username of the sender is also passed
+     * to the method.
+     */
+    void processMessage(const std::string &, const std::string &);
 };
 
-class GameHub
+class GameHub : public Observer
 {
 private:
     /**
      * Needed to see if users are indeed connected and
      * access their handlers.
      */
-    UserHub &m_userHub;
+    UserHub *m_userHub;
     std::vector<GameHandler> m_games;
 
 public:
+    GameHub(UserHub *);
+
+    void update(Event) override;
     /**
      * Create game with two users' usernames.
      */
-    void createGame(const std::string &, const std::string &);
+    void createGame(std::initializer_list<std::string>);
+    void eraseFinished();
 };
