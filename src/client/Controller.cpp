8072cdbd548caef1aee0a486fc4f93c62aedb689
akourieh <Anne-Marie.Kourieh@ulb.be>
2022-02-22 14:21:09 +0100

    handleClick for a Player Action

--- src/client/Controller.cpp
+++ src/client/Controller.cpp
@@ -10,47 +10,20 @@ Controller::Controller(std::shared_ptr<Board> board)
     : board {board} {}
 
 
-void Controller::handleClick(Point p) 
+void Controller::handleClick(Point p)
 {
-    if (board->isPositionValid(p)) 
-    {
-        // if the player is trying to move his pawn
-        if (board->isCell(p)) 
-        {
-            if (click_count == 0) 
-            {   
-                currentPlayer = whichPlayer(p); // jsp si c utile ou bien cmt on fait pr savoir qui est le current player ??
-                first = p;
-                ++click_count;
-            }
-
-            else if (board->isFree(p))
-            {
-                second = p;
-                board->movePlayer(currentPlayer, second);
-                --click_count;
-            }
-        }
-
-        // if the player is trying to place a wall
-        // we send the further left side cell or down side cell of the wall since a wall takes up 2 cells
-        // do we care about who places it ? (to decrement the number of walls for a player ?)
-        if (board->isCorridor(p) && board->isFree(p)) 
-        {
-            // board->placeWall(p, WHAT); ??
-        }
-    }
+    if (board->isCell(p)) handleClickPlayer(p);         // voir en fonction de la réprésentation visuel faite par
+    else (board->isCorridor(p)) handleClickWall(p);     // Léo et Sacha car problème de conversion sinon
 }
 
 
-std::shared_ptr<Player> whichPlayer(Point p) 
+void Controller::handleClickPlayer(Point p) 
 {
-    if (board->isCell(p))
-    {
-        if (!board->isFree(p))
-        {
-            // how find pointer to player
-            // make function in board which gives pointer to board component
-        }
-    }
+    PlayerAction action{board, players[currentPlayerIndex], p};  // Question ? is p a position in the matrix or a position in the view of the players
+    if (action.executeAction()) currentPlayerIndex = (currentPlayerIndex+1) % nPlayers;                    // because to remember : conversion or not ?
+}
+
+void Controller::handleClickWall(Point p) 
+{
+    // Elias
 }
