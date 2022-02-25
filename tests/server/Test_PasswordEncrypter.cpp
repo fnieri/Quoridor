40c4d09227767a4fe1480276959c3917aa013cfb
fnieri <francesco.nieri@ulb.be>
2022-02-25 16:43:40 +0100

    Finally implemented catch, thx boris, reverted cryptopp dep

--- tests/server/Test_PasswordEncrypter.cpp
+++ tests/server/Test_PasswordEncrypter.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch2.hpp>
+#include <catch2/catch.hpp>
 
 #include "src/server/PasswordEncrypter.h"
 
@@ -9,23 +9,22 @@ SCENARIO("Encrypting upon registering")
 {
     GIVEN("An encrypter that generates two passwords 1 second apart")
     {
-        std::unique_ptr<PasswordEncrypter> anEncrypter = PasswordEncrypter::Instance();
         
-        std::vector<std::string> firstRegisterHash = anEncrypter->registerEncrypt("password123");
+        std::vector<std::string> firstRegisterHash = PasswordEncrypter::Instance()->registerEncryption("password123");
         sleep(1);
-        std::vector<std::string> secondRegisterHash = anEncrypter->registerEncrypt("password123");
+        std::vector<std::string> secondRegisterHash = PasswordEncrypter::Instance()->registerEncryption("password123");
         
         THEN("Salt keys and digests should be different")
         {
-            REQUIRE_FALSE(firstRegisterHash[0], secondRegisterHash[0]);
-            REQUIRE_FALSE(firstRegisterHash[1], secondRegisterHash[1]);
+            REQUIRE_FALSE(firstRegisterHash[0] == secondRegisterHash[0]);
+            REQUIRE_FALSE(firstRegisterHash[1] == secondRegisterHash[1]);
         }
     }
     GIVEN("Two hashes generated in the same second")
     {
         THEN("Salt Keys and hashes should be the same")
         {
-            REQUIRE(anEncrypter.registerEncrypt("password123") == anEncrypter("password123"));
+            REQUIRE(PasswordEncrypter::Instance()->registerEncryption("password123") == PasswordEncrypter::Instance()->registerEncryption("password123"));
         }
     }
 }
@@ -34,15 +33,13 @@ SCENARIO("Encrypting upon login")
 {
     GIVEN("SaltKey + passwordEncrypt with known digest")
     {
-        std::unique_ptr<PasswordEncrypter> anEncrypter = PasswordEncrypter::Instance();
-        
         THEN("Hashing should return true")
         {
-          REQUIRE(anEncrypter.compareHash("password123", "1234567890", "F6211D8C3930C89E962D5B293541D3113D95F9CED1DF14CA96517B98F03B31D9") == true)
+          REQUIRE(PasswordEncrypter::Instance()->compareHash("password123", "1234567890", "F6211D8C3930C89E962D5B293541D3113D95F9CED1DF14CA96517B98F03B31D9") == true);
         }
         THEN("By changing salt key, digest should be the same")
         {
-          REQUIRE(anEncrypter.compareHash("password123", "1234567891", "F6211D8C3930C89E962D5B293541D3113D95F9CED1DF14CA96517B98F03B31D9") == false);
+          REQUIRE(PasswordEncrypter::Instance()->compareHash("password123", "1234567891", "F6211D8C3930C89E962D5B293541D3113D95F9CED1DF14CA96517B98F03B31D9") == false);
         }
     }
 }
