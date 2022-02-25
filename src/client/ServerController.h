530aeab94c653f7fbbf20594662cba541618dc0d
akourieh <Anne-Marie.Kourieh@ulb.be>
2022-02-25 20:43:47 +0100

    viewController.cpp and main.cpp (not sure though)

--- src/client/ServerController.h
+++ src/client/ServerController.h
@@ -1,17 +1,25 @@
+/**
+ * @file ServerController.h
+ * @author your name (you@domain.com)
+ * @brief Controller that gets messages from the Server and sends them to the correct Model and also sends messages to the Server gotten from the ViewController
+ * @date 2022-02-25
+ * 
+*/
+
 #pragma once
 
-// Server Controller to easily send actions to the different models' controller : Game + Chat + Network
 class ServerController
 {
 public:
     ServerController();
 
     /* To Game Model */
-    void movePlayer(std::string action);          
+    void movePlayer(std::string action);
+    void placeWall(std::string action);
 
     /* To Chat Model */
-    void sendMessage(std::string receiver, std::string msg, int gameId);              
-    void receiveMessage(std::string receiver, std::string msg, int gameId);           
+    void sendMessage(std::string receiver, std::string msg, int gameId);
+    void receiveMessage(std::string receiver, std::string msg, int gameId);
     void loadMessages(std::string receiver);
     void loadMessages(int gameId);
 
@@ -20,17 +28,16 @@ public:
     void saveGame();
     void pauseGame();
 
-    void sendFriendRequest(std::string receiver);      
+    void sendFriendRequest(std::string receiver);
     void checkLeaderBoard();
 
-        // - Multiplayers Games
-    void sendInvite(std::string aFriend, std::string gameSetup);      
-    void joinGame(int gameId);      
-    void askPause(std::string aFriend);   // a friend asks to pause
+    // - Multiplayers Games
+    void sendInvite(std::string aFriend, std::string gameSetup);
+    void joinGame(int gameId);
+    void askPause(std::string aFriend); // a friend asks to pause
 
-        // - Login
+    // - Login
     void registerPlayer(std::string username, std::string password);
     void logIn(std::string username, std::string password);
-    void logOut(std::string username, std::string password);
-
+    void logOut();
 };
--- src/client/ViewController.h
+++ src/client/ViewController.h
@@ -1,4 +1,19 @@
+/**
+ * @file ViewController.h
+ * @author Kourieh Anne-Marie
+ * @brief Controller that gets messages from the View and sends them to the correct Model
+ * @date 2022-02-25
+ * 
+*/
+
 #pragma once
+
+
+#include "Board.h"
+#include "Player.h"
+#include "Point.h"
+#include "WallEnum.h"
+#include "ServerController.h"
 
 #include <memory>
 #include <vector>
@@ -8,50 +23,57 @@ class ViewController
 {
 private:
     std::shared_ptr<Board> board;
-    int nPlayers;                                       
-    std::vector<std::shared_ptr<Player>> players; 
-    int currentPlayerIndex=0;                           // index of the current player in the vector of Players
+    int nPlayers;
+    std::vector<std::shared_ptr<Player>> players;
+    std::shared_ptr<ServerController> serverController;
+    int currentPlayerIndex = 0; // index of the current player in the vector of Players
     int currentGameId;
-
+    std::string gameSetup;
 public:
-    ViewController(shared_ptr<Board> board, std::vector<std::shared_ptr<Player>> players, int nPlayers);
+    ViewController();
+    ~ViewController();
+
+    /* Setters */
+    void setBoard(std::shared_ptr<Board> aBoard);
+    void setPlayers(std::vector<std::shared_ptr<Player>> thePlayers);
 
     /* To Game Model */
     /**
      * @brief Plays a PlayerAction, when the current player moves his pawn
-     * 
+     *
      * @param p the position to move the pawn to
      */
-    void movePlayer(Point p); 
-    
+    void movePlayer(Point p);
+
     /**
      * @brief Plays a WallAction, when the current player places one of his walls on the board
-     * 
+     *
      * @param p the corridor in which the wall will be placed
-     * @param orientation 
+     * @param orientation
      */
     void placeWall(Point p, WallOrientation orientation);
 
-    /* To Network Model */
+    /* To Network Model (all of these are to send to the ServerController) */ 
     void registerPlayer(std::string username, std::string password);
     void logIn(std::string username, std::string password);
     void logOut();
 
+    void setGameSetup(std::string gameS);
     void startGame();
     void saveGame();
     void pauseGame();
 
-    void sendInvite(std::string aFriend, std::string gameSetup);      
-    void joinGame(int gameId);      
+    void sendInvite(std::string aFriend, std::string gameSetup);
+    void joinGame(int gameId);
     void askToPause(std::string aFriend);
 
-    void sendFriendRequest(std::string receiver);      
+    void sendFriendRequest(std::string receiver);
     void checkLeaderBoard();
 
-    /* To Chat Model */
+    /* To Chat Model (there isn't yet a chat model) */
     void sendMessage(std::string receiver, std::string msg);
-    void sendMessage(std::string msg, int gameId);              
-    void receiveMessage(std::string receiver, std::string msg, int gameId);           
+    void sendMessage(std::string msg, int gameId);
+    void receiveMessage(std::string receiver, std::string msg, int gameId);
     void loadMessages(std::string friendUsername);
     void loadMessages(int gameId);
 };
