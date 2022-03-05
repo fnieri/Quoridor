b45cb8addb9e4adc5ce2be1a9da75e51475bc613
n3puiol <louis.vanstappen@gmail.com>
2022-03-05 13:45:35 +0100

    check

---
+++ src/common/QuoridorEvent.h
@@ -0,0 +1,12 @@
+/**
+ * @file QuoridorEvent.h
+ * @author Boris Petrov
+ * @brief Events occuring
+ * @date 02/25/22
+ */
+
+#pragma once
+
+enum class QuoridorEvent {
+    Modified, ///< Generic modification
+};
